ad172f43d30be0cd9e0041ef9c270b34cd03b4a7
Sergiy Dybskiy <s@serg.tech>
2025-02-06 17:33:26 +0100

    formatting

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,24 +1,24 @@
-import "dmno/injector-standalone/edge-auto";
-import { Hono } from "hono";
+import 'dmno/injector-standalone/edge-auto';
+import { Hono } from 'hono'
 
-import { HonoEnv, initCommonMiddlewares } from "./lib/middlewares";
-import { agentLibRoutes } from "./routes/agent-lib";
-import { agentRoutes } from "./routes/agents";
-import { authRoutes } from "./routes/auth";
-import { configItemRoutes } from "./routes/config-items";
-import { projectRoutes } from "./routes/project";
+import { HonoEnv, initCommonMiddlewares } from './lib/middlewares';
+import { authRoutes } from './routes/auth';
+import { agentLibRoutes } from './routes/agent-lib';
+import { projectRoutes } from './routes/project';
+import { configItemRoutes } from './routes/config-items';
+import { agentRoutes } from './routes/agents';
 
 const app = new Hono<HonoEnv>();
 
 initCommonMiddlewares(app);
 
-app.get("/", async (c) => {
-  return c.json({ apiStatus: "ok" });
+app.get('/', async (c) => {
+  return c.json({ apiStatus: 'ok' });
 });
-app.route("/", authRoutes);
-app.route("/", agentLibRoutes);
-app.route("/", projectRoutes);
-app.route("/", configItemRoutes);
-app.route("/", agentRoutes);
+app.route('/', authRoutes);
+app.route('/', agentLibRoutes);
+app.route('/', projectRoutes);
+app.route('/', configItemRoutes);
+app.route('/', agentRoutes);
 
-export default app;
+export default app
