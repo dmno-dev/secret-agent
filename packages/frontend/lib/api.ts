1df22dca2a94a202f0c4ba6448d11b8b2d11d9aa
Sergiy Dybskiy <s@serg.tech>
2025-02-13 19:26:13 -0500

    simplified auth back to basics but works for cb and mm

--- packages/frontend/lib/api.ts
+++ packages/frontend/lib/api.ts
@@ -5,21 +5,10 @@ export const AUTH_ID_LOCALSTORAGE_KEY = 'SA_AUTH_ID';
 
 const getAuthInfo = () => {
   if (typeof window === 'undefined') return null;
-  const authToken = window.localStorage.getItem(AUTH_KEY_LOCALSTORAGE_KEY);
-  if (!authToken) return null;
-
-  try {
-    const [signature, timestamp] = authToken.split('|');
-    return {
-      id: window.localStorage.getItem(AUTH_ID_LOCALSTORAGE_KEY)!,
-      auth: signature,
-      timestamp,
-      nonce: window.localStorage.getItem('SA_AUTH_NONCE') || '',
-      uri: DMNO_PUBLIC_CONFIG.SECRETAGENT_WEB_URL,
-    };
-  } catch (e) {
-    return null;
-  }
+  return {
+    id: window.localStorage.getItem(AUTH_ID_LOCALSTORAGE_KEY)!,
+    auth: window.localStorage.getItem(AUTH_KEY_LOCALSTORAGE_KEY)!,
+  };
 };
 
 export const secretAgentApi = ky.extend({
@@ -33,9 +22,6 @@ export const secretAgentApi = ky.extend({
         if (!authInfo) throw new Error('not logged in');
         req.headers.set('sa-admin-auth', authInfo.auth);
         req.headers.set('sa-user-id', authInfo.id);
-        req.headers.set('sa-auth-timestamp', authInfo.timestamp);
-        req.headers.set('sa-auth-nonce', authInfo.nonce);
-        req.headers.set('sa-auth-uri', authInfo.uri);
       },
     ],
   },
